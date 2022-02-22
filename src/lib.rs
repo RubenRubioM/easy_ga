c15ecb3f651fae65432ae0cded5b8f3b68985519
RubenRubioM <rubensipala@gmail.com>
2022-02-22 15:31:13 +0100

    v1.3.2 - License and documentation crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+//! Library to implement genetic algorithms
+
 pub mod gene;
 pub mod genetic_algorithm;
 pub mod selection;
 
 pub use gene::Gene;
 pub use genetic_algorithm::GeneticAlgorithm;
-pub use selection::Selection;
-pub use selection::SelectionAlgorithms;
+pub use selection::*;
